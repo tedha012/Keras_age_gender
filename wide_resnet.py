a083c3cbf35d55f183127f91d9f9e81951b8909d
tedha012 <tedha012@gmail.com>
2022-11-24 17:20:58 +0900

    first commit

--- wide_resnet.py
+++ wide_resnet.py
@@ -6,12 +6,12 @@ import numpy as np
 from keras.models import Model
 from keras.layers import Input, Activation, add, Dense, Flatten, Dropout
 from keras.layers.convolutional import Conv2D, AveragePooling2D
-from keras.layers.normalization import BatchNormalization
+from keras.layers import BatchNormalization
 from keras.regularizers import l2
 from keras import backend as K
 
-sys.setrecursionlimit(2 ** 20)
-np.random.seed(2 ** 10)
+sys.setrecursionlimit(2**20)
+np.random.seed(2**10)
 
 
 class WideResNet:
@@ -23,7 +23,7 @@ class WideResNet:
         self._use_bias = False
         self._weight_init = "he_normal"
 
-        if K.image_dim_ordering() == "th":
+        if K.image_data_format() == "th":
             logging.debug("image_dim_ordering = 'th'")
             self._channel_axis = 1
             self._input_shape = (3, image_size, image_size)
@@ -40,8 +40,7 @@ class WideResNet:
             #               strides="(stride_vertical,stride_horizontal)",
             #               padding="same" or "valid"] ]
             # B(3,3): orignal <<basic>> block
-            conv_params = [[3, 3, stride, "same"],
-                           [3, 3, (1, 1), "same"]]
+            conv_params = [[3, 3, stride, "same"], [3, 3, (1, 1), "same"]]
 
             n_bottleneck_plane = n_output_plane
 
@@ -56,42 +55,50 @@ class WideResNet:
                         convs = BatchNormalization(axis=self._channel_axis)(net)
                         convs = Activation("relu")(convs)
 
-                    convs = Conv2D(n_bottleneck_plane, kernel_size=(v[0], v[1]),
-                                          strides=v[2],
-                                          padding=v[3],
-                                          kernel_initializer=self._weight_init,
-                                          kernel_regularizer=l2(self._weight_decay),
-                                          use_bias=self._use_bias)(convs)
+                    convs = Conv2D(
+                        n_bottleneck_plane,
+                        kernel_size=(v[0], v[1]),
+                        strides=v[2],
+                        padding=v[3],
+                        kernel_initializer=self._weight_init,
+                        kernel_regularizer=l2(self._weight_decay),
+                        use_bias=self._use_bias,
+                    )(convs)
                 else:
                     convs = BatchNormalization(axis=self._channel_axis)(convs)
                     convs = Activation("relu")(convs)
                     if self._dropout_probability > 0:
                         convs = Dropout(self._dropout_probability)(convs)
-                    convs = Conv2D(n_bottleneck_plane, kernel_size=(v[0], v[1]),
-                                          strides=v[2],
-                                          padding=v[3],
-                                          kernel_initializer=self._weight_init,
-                                          kernel_regularizer=l2(self._weight_decay),
-                                          use_bias=self._use_bias)(convs)
+                    convs = Conv2D(
+                        n_bottleneck_plane,
+                        kernel_size=(v[0], v[1]),
+                        strides=v[2],
+                        padding=v[3],
+                        kernel_initializer=self._weight_init,
+                        kernel_regularizer=l2(self._weight_decay),
+                        use_bias=self._use_bias,
+                    )(convs)
 
             # Shortcut Connection: identity function or 1x1 convolutional
             #  (depends on difference between input & output shape - this
             #   corresponds to whether we are using the first block in each
             #   group; see _layer() ).
             if n_input_plane != n_output_plane:
-                shortcut = Conv2D(n_output_plane, kernel_size=(1, 1),
-                                         strides=stride,
-                                         padding="same",
-                                         kernel_initializer=self._weight_init,
-                                         kernel_regularizer=l2(self._weight_decay),
-                                         use_bias=self._use_bias)(net)
+                shortcut = Conv2D(
+                    n_output_plane,
+                    kernel_size=(1, 1),
+                    strides=stride,
+                    padding="same",
+                    kernel_initializer=self._weight_init,
+                    kernel_regularizer=l2(self._weight_decay),
+                    use_bias=self._use_bias,
+                )(net)
             else:
                 shortcut = net
 
             return add([convs, shortcut])
 
         return f
-
 
     # "Stacking Residual Units on the same stage"
     def _layer(self, block, n_input_plane, n_output_plane, count, stride):
@@ -103,41 +110,73 @@ class WideResNet:
 
         return f
 
-#    def create_model(self):
+    #    def create_model(self):
     def __call__(self):
         logging.debug("Creating model...")
 
-        assert ((self._depth - 4) % 6 == 0)
+        assert (self._depth - 4) % 6 == 0
         n = (self._depth - 4) / 6
 
         inputs = Input(shape=self._input_shape)
 
         n_stages = [16, 16 * self._k, 32 * self._k, 64 * self._k]
 
-        conv1 = Conv2D(filters=n_stages[0], kernel_size=(3, 3),
-                              strides=(1, 1),
-                              padding="same",
-                              kernel_initializer=self._weight_init,
-                              kernel_regularizer=l2(self._weight_decay),
-                              use_bias=self._use_bias)(inputs)  # "One conv at the beginning (spatial size: 32x32)"
+        conv1 = Conv2D(
+            filters=n_stages[0],
+            kernel_size=(3, 3),
+            strides=(1, 1),
+            padding="same",
+            kernel_initializer=self._weight_init,
+            kernel_regularizer=l2(self._weight_decay),
+            use_bias=self._use_bias,
+        )(
+            inputs
+        )  # "One conv at the beginning (spatial size: 32x32)"
 
         # Add wide residual blocks
         block_fn = self._wide_basic
-        conv2 = self._layer(block_fn, n_input_plane=n_stages[0], n_output_plane=n_stages[1], count=n, stride=(1, 1))(conv1)
-        conv3 = self._layer(block_fn, n_input_plane=n_stages[1], n_output_plane=n_stages[2], count=n, stride=(2, 2))(conv2)
-        conv4 = self._layer(block_fn, n_input_plane=n_stages[2], n_output_plane=n_stages[3], count=n, stride=(2, 2))(conv3)
+        conv2 = self._layer(
+            block_fn,
+            n_input_plane=n_stages[0],
+            n_output_plane=n_stages[1],
+            count=n,
+            stride=(1, 1),
+        )(conv1)
+        conv3 = self._layer(
+            block_fn,
+            n_input_plane=n_stages[1],
+            n_output_plane=n_stages[2],
+            count=n,
+            stride=(2, 2),
+        )(conv2)
+        conv4 = self._layer(
+            block_fn,
+            n_input_plane=n_stages[2],
+            n_output_plane=n_stages[3],
+            count=n,
+            stride=(2, 2),
+        )(conv3)
         batch_norm = BatchNormalization(axis=self._channel_axis)(conv4)
         relu = Activation("relu")(batch_norm)
 
         # Classifier block
         pool = AveragePooling2D(pool_size=(8, 8), strides=(1, 1), padding="same")(relu)
         flatten = Flatten()(pool)
-        predictions_g = Dense(units=2, kernel_initializer=self._weight_init, use_bias=self._use_bias,
-                              kernel_regularizer=l2(self._weight_decay), activation="softmax")(flatten)
-        predictions_a = Dense(units=101, kernel_initializer=self._weight_init, use_bias=self._use_bias,
-                              kernel_regularizer=l2(self._weight_decay), activation="softmax")(flatten)
+        predictions_g = Dense(
+            units=2,
+            kernel_initializer=self._weight_init,
+            use_bias=self._use_bias,
+            kernel_regularizer=l2(self._weight_decay),
+            activation="softmax",
+        )(flatten)
+        predictions_a = Dense(
+            units=101,
+            kernel_initializer=self._weight_init,
+            use_bias=self._use_bias,
+            kernel_regularizer=l2(self._weight_decay),
+            activation="softmax",
+        )(flatten)
 
         model = Model(inputs=inputs, outputs=[predictions_g, predictions_a])
 
         return model
-
